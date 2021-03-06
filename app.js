db18611f6b6db6d752eeb310db465f9efa0f7af1
Muhammad Junaid Aslam <muhammad.aslam@tuhh.de>
2021-03-06 19:23:04 +0100

    Done with video #4, Added type event and type input in the application

--- app.js
+++ app.js
@@ -4,18 +4,36 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+// To store temporarity, just for testing
+const events = [];
+
 app.use(express.json());
 
 app.use(
     '/graphql', 
     graphqlHTTP({
         schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
 
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
 
             schema {
@@ -25,11 +43,18 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+                return events;
             },
             createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price, // + sign to convert it to float
+                    date: args.eventInput.date
+                };
+                events.push(event);
+                return event
             }
         },
         graphiql: true
