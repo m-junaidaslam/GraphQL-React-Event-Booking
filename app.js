eda97800eaaeeda4d91afdca226fa64b5d844791
Muhammad Junaid Aslam <junaidaslam797@gmail.com>
2021-03-09 17:57:32 +0100

    Till Video 6 Adding Relations | Build a Complete App with GraphQL, Node.js, MongoDB and React.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event')
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,11 +23,22 @@ app.use(
                 date: String!
             }
 
+            type User {
+                _id: ID!
+                email: String!
+                password: String
+            }
+
             input EventInput {
                 title: String!
                 description: String!
                 price: Float!
                 date: String!
+            }
+
+            input UserInput {
+                email: String!
+                password: String!
             }
 
             type RootQuery {
@@ -34,6 +47,7 @@ app.use(
 
             type RootMutation {
                 createEvent(eventInput: EventInput): Event
+                createUser(userInput: UserInput): User
             }
 
             schema {
@@ -70,6 +84,24 @@ app.use(
                         console.log(err);
                         throw err;
                     });
+            },
+            createUser: args => {
+                return bcrypt
+                    .hash(args.userInput.password, 12)
+                    .then(hashedPassword => {
+                        const user = new User({
+                            email: args.userInput.email,
+                            password: hashedPassword
+                        });
+                        return user.save();
+                    })
+                    .then(result => {
+                        return { ...result._doc, _id: result.id };
+                    })
+                    .catch(err => {
+                        throw err;
+                    });
+                
             }
         },
         graphiql: true
